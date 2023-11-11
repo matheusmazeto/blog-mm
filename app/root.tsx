5630607bb49cb589acbd9c6537a878ef871a97be
matheusmazeto <mgmazeto@gmail.com>
2023-11-11 15:45:22 +0000

    add eslint

--- app/root.tsx
+++ app/root.tsx
@@ -1,5 +1,5 @@
-import { cssBundleHref } from "@remix-run/css-bundle";
-import type { LinksFunction } from "@remix-run/node";
+import { cssBundleHref } from '@remix-run/css-bundle'
+import type { LinksFunction } from '@remix-run/node'
 import {
   Links,
   LiveReload,
@@ -7,14 +7,14 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
-} from "@remix-run/react";
+} from '@remix-run/react'
 
-import styles from "./tailwind.css";
+import styles from './tailwind.css'
 
 export const links: LinksFunction = () => [
-  ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
-  { rel: "stylesheet", href: styles }
-];
+  ...(cssBundleHref ? [{ rel: 'stylesheet', href: cssBundleHref }] : []),
+  { rel: 'stylesheet', href: styles },
+]
 
 export default function App() {
   return (
@@ -32,5 +32,5 @@ export default function App() {
         <LiveReload />
       </body>
     </html>
-  );
+  )
 }
